5811872c18f67dfd3525524b5e8137631713928e
MarcosNass <jmarcosnfass93@hotmail.com>
2021-03-11 18:45:04 -0300

    Projeto iniciado

---
+++ src/Livros.java
@@ -0,0 +1,66 @@
+
+public class Livros {
+	private String title;
+	private String isbn;
+	private String author;
+	private String edit;
+	private String yearPub;
+	
+	//comment
+	//other comment
+	
+	public Livros(String t, String i, String a, String e, String y) {
+		setTitle(t);
+		setIsbn(i);
+		setAuthor(a);
+		setEdit(e);
+		setYearPub(y);
+	}
+	
+	public void setTitle(String t) {
+		this.title = t;
+	}
+	
+	public String getTitlte() {
+		return title;
+	}
+	
+	public void setIsbn(String i) {
+		this.isbn = i;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setAuthor(String a) {
+		this.author = a;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setEdit(String e) {
+		this.edit = e;
+	}
+	
+	public String getEdit() {
+		return edit;
+	}
+	
+	public void setYearPub(String y) {
+		this.yearPub = y;
+	}
+	
+	public String getYearPub() {
+		return yearPub;
+	}
+	
+	
+	public void displayBook() {
+		System.out.print("Title: " + this.title + " - Author: " + this.author + "  - Publishing Company: " + this.yearPub);
+	}
+	
+
+}
---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Livros book = new Livros("A Cabana", "Zé", "Figures", "1245124578", "2005");		
+		Livros book1 = new Livros("A casa", "Pedro", "Figures Net Work", "9985568", "2010");
+		
+		book.displayBook();
+		System.out.println();
+		book1.displayBook();
+	    
+		
+
+	}
+
+}
