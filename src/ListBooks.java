ced27585d3c534f0dfdd44412f2f173ddb54ddcb
MarcosNass <jmarcosnfass93@hotmail.com>
2021-03-14 20:58:00 -0300

    bug ao usar a biblioteca Scanner, a leitura não era feita corretamente e o código inteiro da classe main foi resetado

--- src/ListBooks.java
+++ src/ListBooks.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-
 public class ListBooks extends Books {
 	private Books[] book;
 	private int lenght;
 	
+	//Construct of class ListBooks
 	public ListBooks(int qt) {
 		this.book = new Books[qt];
 		this.lenght = 0;
 	}
-	
+	//Add book in list
 	public void addBook(Books b) throws Exception {
 		if (this.lenght < this.book.length) {
 			this.book[this.lenght] = b;
--- src/main.java
+++ src/main.java
@@ -1,29 +1,10 @@
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collections;
 
 public class main {
 	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-	
-		ListBooks l = new ListBooks(4);
-		try {
-			Books b1 = new Books();
-			b1.setTitle("A cabana");
-			b1.setAuthor("Pedro");
-			l.addBook(b1);
-			l.toString();
-			Books b2 = new Books();
-			b2.setTitle("A casa");
-			b2.setAuthor("Zé");
-			l.addBook(b2);
-			l.toString();
-			
-		} catch (Exception e){
-			e.printStackTrace();
-		}
+	public static void main(final String[] args) {
+
 		
 		
 		
